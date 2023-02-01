7d1a6e236e7786018c64f1329dc12c98ca61a733
HeoJiye <heoze1031@gmail.com>
2023-02-01 18:04:44 +0900

    fix Cannot GET

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const app = express()
-const PORT = 8001
+const express = require('express');
+const app = express();
+const PORT = 8001;
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use('/Board', express.static(__dirname + '/views/Forum'));
 app.use('/Header', express.static(__dirname + '/views/Header'));
 app.use('/Main', express.static(__dirname + '/views/Main'));
-app.use('/static', express.static(__dirname + '/views/static'));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
 app.listen(PORT, () => {
     console.log(`server started on PORT ${PORT}`);
 });
